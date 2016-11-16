b31e79b960cf9c017b4da7b8297166127d61f822
julantinojaftha <julantino@Julans-MacBook-Pro.local>
2016-11-16 16:25:35 +0200

    Completed

--- Palindrome.java
+++ Palindrome.java
@@ -10,30 +10,23 @@ class Palindrome {
 
         int len = word.length();
 
-        if (len % 2 != 0) {
+        if (len % 2 != 0)
             len -= 1;
-        }
          else
              numType = 1;
 
         for (int i=0;i<len/2;i++)
         {
-           if (  word.toLowerCase().charAt(i) == word.toLowerCase().charAt(len-i-numType)) {
-
+           if (word.toLowerCase().charAt(i) == word.toLowerCase().charAt(len-i-numType))
                test = true;
-           }else {
+           else {
                test = false;
-
                break;
            }
         }
-
-
-
         return test;
     }
 }
-
 
 public class Main {
     public static void main(String[] args)
